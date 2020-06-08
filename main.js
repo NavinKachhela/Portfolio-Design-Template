70821e3f642fee7faefb158eefe8c664db3d9c85
Navin H Kachhela <kachhelanavin@gmail.com>
2020-06-08 14:18:26 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,94 @@
+$("#button1").click(function () {
+    let output = `<div class="collapse" id="collapseExample">
+                <div class="card card-body">
+Smart parking application
+Analysis of different technique for detection of SQL injection
+Issues in IoT healthcare platforms: A critical study and review
+                </div>
+            </div>`
+    document.getElementById("output").innerHTML = output;
+});
+
+$("#button2").click(function () {
+    let output = `<div class="collapse" id="collapseExample">
+                <div class="card card-body">
+       Published Demand Hub: a Better Way to Meet Your Daily Needs! in Journal Available at SSRN 3565517 in 2020.
+
+Published Security of 6LoWPAN IoT networks in hospitals for medical data exchange in Journal Procedia Computer Science in 2019.
+
+Published MAC-Based Group Management Protocol for IoT [MAC GMP-IoT] in Journal Proceedings of International Conference on Wireless Communication in 2018.
+                </div>
+            </div>`
+    document.getElementById("output").innerHTML = output;
+});
+$("#button3").click(function () {
+    let output = `<div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    Certified for ioT trainer.
+
+Microsoft Certification for “Cryptography and System Security”.
+
+NPTEL certification for “System Security”.
+
+VESIT
+
+                </div>
+            </div>`
+    document.getElementById("output").innerHTML = output;
+});
+$("#button4").click(function () {
+    let output = `<div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    Use of internet of things (iot) in healthcare: A survey
+        Security of 6LoWPAN IoT networks in hospitals for medical data exchange
+
+A Robust Scheme for Secure Communication in Internet of Things
+\
+                </div>
+            </div>`
+    document.getElementById("output").innerHTML = output;
+});
+$("#button5").click(function () {
+    let output = `<div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                   A Proposal for Epidemic Prediction using Deep Learning
+WiFi Authentication through Social Networks
+Conference on Recent Trends in Computer ,IOSR Journal of Computer …		
+Smart Nursing Home Patient Monitoring System
+                </div>
+            </div>`
+    document.getElementById("output").innerHTML = output;
+});
+$("#button6").click(function () {
+    let output = `<div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    Android (operating system), Internet of Things, biomedical communication, gesture recognition, handicapped aids, health care, medical computing, mobile computing, object recognition
+                </div>
+            </div>`
+    document.getElementById("output").innerHTML = output;
+});
+document.getElementById("navbar").style.display = "none";
+var $j = jQuery.noConflict();
+$j(window).on('load', function () {
+
+    $j("#preloader").delay(4000).fadeOut('slow');
+
+
+});
+// $j(window).on('load',function () {
+//    output = document.getElementById("navbar").style.transitionDelay = "10s";
+//     if(output)
+//         {
+//          
+//            document.getElementById("navbar").style.display="block";
+//         }
+//});
+setTimeout(function () {
+    document.getElementById("navbar").style.display = "block";
+}, 4200);
+$j("#submit1").click(function () {
+    let output = `<div class="alert alert-success" role="alert">
+  Request Successfully Submitted!!
+</div>`
+    document.getElementById("output1").innerHTML = output;
+});
